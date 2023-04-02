c1027fd8bcb0b8c9e6d3aa365b5c8bc5386951da
NagaVaishnavi-Puppala <puppala.n@northeastern.edu>
2023-04-02 13:30:36 -0400

    react app uploaded

---
+++ frontend/my-app/src/Main/MainPage.js
@@ -0,0 +1,22 @@
+// import React from "react";
+// import "./MainPage.css"
+// import {Link}from "react-router-dom"
+
+// export default function MainPage(){
+//     return (
+//         <div>
+//             <nav>
+//                 <Link to='/home' >Home</Link>
+//                 <Link to='/about' >About</Link>
+//                 <Link to='/profile' >Profile</Link>
+//                 <Link to='/contact' >Contact</Link>
+//                 <Link to='/jobs' >Jobs</Link>
+//                 <Link to='/list' >Jobs</Link>
+//                 <Link to='/content' >Jobs</Link>
+//             </nav>
+//             <footer>
+//                 <div>This is a footer</div>
+//             </footer>
+//         </div>
+//     )
+//}
